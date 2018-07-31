0e6864f6e7df5b56e6337a1056837e49db5a63ab
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-07-31 06:59:17 +0200

    More states on letters. Changed scoring.

--- wordball/ball.go
+++ wordball/ball.go
@@ -93,7 +93,7 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "collision"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
@@ -103,7 +103,6 @@ embedded_components {
   "group: \"letter\"\n"
   "mask: \"wall\"\n"
   "mask: \"letter\"\n"
-  "mask: \"cursor\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
@@ -205,6 +204,51 @@ embedded_components {
     x: 27.0
     y: -25.0
     z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cursor"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.2\n"
+  "group: \"letter\"\n"
+  "mask: \"cursor\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 82.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 1.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
   }
   rotation {
     x: 0.0
